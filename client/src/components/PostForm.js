97c2ec7a871c81251ede2a5758ef2fab6ad01793
katherinestout <kjs14@hood.edu>
2019-07-08 13:16:53 -0400

    react done

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -7,24 +7,50 @@ class PostForm extends Component {
             title: '',
             body: ''
         };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+        fetch('https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        }).then(res => res.json())
+        .then(data => console.log(data));
     }
     render() {
         return (
             <div>
                 <h1>Post Form</h1>
-                <form>
+                <form onSubmit = {this.onSubmit}>
                     <div>
                         <label>Title:</label>
                         <br/>
                         <input type="text"
                         name="title"
-                        value={this.state.title}/>
+                        value={this.state.title}
+                        onChange={this.onChange}/>
                     </div>
                     <div>
                         <label>Body:</label>
                         <br/>
                         <textarea name="body"
-                        value={this.state.body}/>
+                        value={this.state.body}
+                        onChange={this.onChange}/>
                     </div>
                     <br/>
                     <button type ="submit">

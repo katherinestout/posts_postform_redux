72125547212add6607e9c052009edd9a57c4e4f8
katherinestout <kjs14@hood.edu>
2019-07-08 14:07:24 -0400

    devtools extension

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -6,8 +6,15 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, 
+const store = createStore(
+    rootReducer, 
     initialState, 
-    applyMiddleware(...middleware));
-
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    
+       
+    );
+    
 export default store;

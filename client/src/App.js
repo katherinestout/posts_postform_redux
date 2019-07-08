675e9c7713111da0fed240e6a9fc8145097b51a6
katherinestout <kjs14@hood.edu>
2019-07-08 10:23:16 -0400

    first

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+       <PostForm/>
+      <Posts/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/PostForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: '',
+            body: ''
+        };
+    }
+    render() {
+        return (
+            <div>
+                <h1>Post Form</h1>
+                <form>
+                    <div>
+                        <label>Title:</label>
+                        <br/>
+                        <input type="text"
+                        name="title"
+                        value={this.state.title}/>
+                    </div>
+                    <div>
+                        <label>Body:</label>
+                        <br/>
+                        <textarea name="body"
+                        value={this.state.body}/>
+                    </div>
+                    <br/>
+                    <button type ="submit">
+                        Submit
+                    </button>
+                </form>
+                
+            </div>
+        )
+    }
+}
+export default PostForm;
